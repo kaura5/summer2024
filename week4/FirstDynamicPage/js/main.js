e8c528a80f2f975b82dd01977243ae0c9e682342
SolCaruso <caruso@ocelot.ltd>
2024-06-06 17:14:24 -0400

    carms first commit

---
+++ week4/FirstDynamicPage/js/main.js
@@ -0,0 +1,24 @@
+let cards = document.querySelector('.cards');
+
+df = new DocumentFragment();
+
+data.results.forEach(function (item) {
+
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    let cardInfo = `
+    <div class="product">
+    <img src=${item.thumbnail} alt=${item.title}>
+    </div>
+        <p>$${item.price}.00</p>
+        <h2>${item.title}</h2>
+    </div>
+    `
+
+    card.innerHTML = cardInfo;
+
+    df.append(card);
+});
+
+cards.append(df);
