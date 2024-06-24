75e10e6fd20896507641d9334de7725f14c2ccca
SolCaruso <caruso@ocelot.ltd>
2024-06-23 23:00:51 -0400

    updates number generator counter

--- week4/RandomNumberGame/main.js
+++ week4/RandomNumberGame/main.js
@@ -1,33 +1,89 @@
 function init() {
+
   let randomNumber = Math.floor(Math.random() * 100) + 1;
   let guessButton = document.getElementById("guessButton");
   let inputNum = document.getElementById("inputNum");
 
   guessButton.addEventListener("click", guessNumber);
 
-  function guessNumber(ev) {
-    ev.preventDefault();
-    let value = inputNum.value.trim();
-    if (value !== "") {
-      // if (value !== "" && value > 0 && value < 101) {
-      let guess = parseInt(value);
-      if (guess > randomNumber) {
-        alert("Too high, try again!");
-      } else if (guess < randomNumber) {
-        alert("Too low, try again!");
-      } else if (isNaN(guess)) {
-        alert("Please enter a number");
-      } else {
-        alert("Congratulations! You guessed the right number!");
-        randomNumber = Math.floor(Math.random() * 100) + 1;
-      }
-    } else {
-      alert("Please enter a number between 1 and 100!");
+
+  let df = new DocumentFragment();
+
+    function counter() {
+        let count = 0;
+
+        return {
+
+            increase: function() {
+                count ++;
+                return count;
+            },
+
+            current: function() {
+                return count;
+            },
+
+            reset: function() {
+                count = 0;
+                return count;
+            },
+
+        };
     }
-    inputNum.value = "";
-  }
+
+    let attempt = document.getElementById('attempt');
+    const myCounter = counter();
+    attempt.append(df);
+
+
+    function updateAttempt() {
+        myCounter.increase();
+        attempt.innerHTML =  myCounter.current();
+        let message = document.getElementById('message');
+        message.innerHTML = "Current attempt..";
+    }
+
+    function resetAttempt() {
+        myCounter.reset();
+        attempt.innerHTML = "";
+        message.innerHTML = "";
+    }
+
+
+    function guessNumber(ev) {
+
+        ev.preventDefault();
+        let value = inputNum.value.trim();
+
+        if (value !== "" && value > 0 && value < 101) {
+            
+        let guess = parseInt(value);
+
+        if (guess > randomNumber) {
+            alert("Too high, try again!");
+            inputNum.value = "";
+            updateAttempt();
+        } else if (guess < randomNumber) {
+            alert("Too low, try again!");
+            inputNum.value = "";
+            updateAttempt();
+        } else {
+            alert("Congratulations! You guessed the right number!");
+            randomNumber = Math.floor(Math.random() * 100) + 1;
+            inputNum.value = "";
+            resetAttempt();
+        }
+        } 
+        
+        else {
+        alert("Please enter a number between 1 and 100!");
+        inputNum.value = "";
+        }
+        
+    }
+
 }
 
 window.addEventListener("DOMContentLoaded", init);
 
-//misiing how many guesses a user took to win
+
