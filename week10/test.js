1107c8ccc74f228cd735c9313606eff59f308c9d
Anoop <kaur0550@algonquinlive.com>
2024-07-21 16:54:24 -0400

    side project

---
+++ week10/test.js
@@ -0,0 +1,14 @@
+let guests = [];
+let nameElement = document.querySelectorAll("div._name_em4437");
+let subInfoElement = document.querySelectorAll("div._sub-info_em4437");
+for (let i = 0; i < nameElement.length; i++) {
+  let customerName = nameElement[i].textContent.trim();
+  let subInfo = subInfoElement[i].textContent.trim();
+  let [phoneNumber, email] = subInfo.split(" • ");
+  guests.push({
+    customerName: customerName,
+    phoneNumber: phoneNumber,
+    email: email,
+  });
+}
+console.log("GuestList", guests);
